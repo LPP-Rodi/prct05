abac62cb3eb33aaf9e08926145d46c68c2acccea
Jacobo Rodicio <alu0100836059@ull.edu.es>
2016-05-27 03:10:04 +0100

    Tarea test como default para Rake creada

--- lib/prct05.rb
+++ lib/prct05.rb
@@ -1,5 +1,6 @@
 require "prct05/version"
 
 module Prct05
-  # Your code goes here...
+
+  
 end
---
+++ spec/prtc05_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'prct05'
+
+describe MiGema do
+  it "Se requieren más tests"
+end
